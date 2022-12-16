cb1472addfb4c6caa3bab6322da6c960871994ac
chogerlate <siwaratlaopromger4444@gmail.com>
2022-12-17 03:55:51 +0700

    changed spacing update

--- main.c
+++ main.c
@@ -20,26 +20,28 @@
 //   printf("%s \n", ( (char*)listGetAt(book->option, 1)) );
 // }
 
-int main(void) {
+int main(void)
+{
 
   // Create Starter-BookList List
   LIST_T *BookList = listCreate(1);
-  char *filepath = "data.txt"; // 
-  // bookCreateFromtxt(BookList, filepath); // แบบมี starter 
+  char *filepath = "data.txt"; //
+  // bookCreateFromtxt(BookList, filepath); // แบบมี starter
   bookCreateFromtxt(BookList, "output.txt"); // โหลด memory from saved file
-  
+
   // Create Borrow Book History List
   LIST_T *BorrowList = listCreate(1);
   char *filepath_borrow = "borrow.txt";
   borrowListCreateFromtxt(BorrowList, filepath_borrow);
-  
+
   // BOOK *b1 = bookCreate();
   // listAppend(BookList, b1);
   // printf("Test from main: %s\n", getBookByIndex(BookList, 0)->title);
 
-// ค่าตัวเลือก
+  // ค่าตัวเลือก
 
-  do {
+  do
+  {
     int selected_choice;
     printf("Menu \n");
     printf("1)Add book\n");
@@ -53,33 +55,41 @@ int main(void) {
     scanf("%d", &selected_choice);
     printf("\n");
     // printf("selected:%d \n",selected_choice);
-    if (selected_choice == 1) {
+    if (selected_choice == 1)
+    {
       BOOK *b1 = bookCreate();
       listAppend(BookList, b1);
-    } else if (selected_choice == 2) {
+    }
+    else if (selected_choice == 2)
+    {
       bookListRemoveByTitle(BookList);
-      bookListShow(BookList,BorrowList);
+      bookListShow(BookList, BorrowList);
     }
 
-    else if (selected_choice == 3){
+    else if (selected_choice == 3)
+    {
       bookEdit(BookList);
     }
-    else if (selected_choice == 4) {
-      bookListShow(BookList,BorrowList);
-    } else if (selected_choice == 5)
+    else if (selected_choice == 4)
+    {
+      bookListShow(BookList, BorrowList);
+    }
+    else if (selected_choice == 5)
       ;
     else if (selected_choice == 6)
       ;
     else if (selected_choice == 7)
       ;
-    else if (selected_choice == 8){
-            saveBookList(BookList, "output.txt");
-            saveBorrowList(BorrowList, filepath_borrow);
-            break;
+    else if (selected_choice == 8)
+    {
+      saveBookList(BookList, "output.txt");
+      saveBorrowList(BorrowList, filepath_borrow);
+      break;
     }
-   else {
-           continue;
-   }
+    else
+    {
+      continue;
+    }
     printf("\n");
   } while (1);
 
